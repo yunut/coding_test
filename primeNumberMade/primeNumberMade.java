a93d94041213d59c0ab1401f21a56d1d6219ad65
yunut <somewheres@nate.com>
2020-02-17 23:00:46 +0900

    coding test update

--- primeNumberMade/primeNumberMade.java
+++ primeNumberMade/primeNumberMade.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 public class primeNumberMade {
 	
-	private static HashSet<Integer> set = new HashSet<>();
-    private static int max;
+	private static int answer;
+	private static ArrayList<Integer> arr;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -14,39 +14,34 @@ public class primeNumberMade {
 	}
 	
 	public static int solution(int[] nums) {
-        int answer = 0;
+        answer = 0;
         int target=0;
-        max = 0;
         
         for(int i=0;i<nums.length;i++) {
             dfs(nums,0,0,i);
         }
         
-        Iterator<Integer> it = set.iterator();
-        ArrayList<Integer> arr = new ArrayList<>();
-        for(int i=2;i<=max;i++) {
+        arr = new ArrayList<>();
+        
+        for(int i=2;i<=1500;i++) {
             arr.add(i);
         }
-        for(int i=2;i<=max/2;i++) {
+        
+        for(int i=2;i<=1500;i++) {
             for(int ii=0;ii<arr.size();ii++) {
                 if(arr.get(ii) != i && arr.get(ii) % i == 0) {
                     arr.set(ii,0);
                 }
             }
         }
-        while(it.hasNext()) {
-            if(arr.contains(it.next())) {
-                answer++;
-            } 
-        }
-        System.out.println(answer);
         return answer;
     }
 	
 	public static void dfs(int[] nums,int sum,int count,int index) {
         if(count == 3) {
-            if(max < sum) max = sum;
-            set.add(sum);
+        	if(arr.contains(sum)) {
+        		answer++;
+        	}
             return;
         }
         
