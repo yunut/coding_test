51f259a43ecaf18c5065769fd42f44a38d7a43e9
yunut <somewheres@nate.com>
2020-03-13 16:38:13 +0900

    coding test update

--- SearchPrimeNumber/SerchPrimeNumber.java
+++ SearchPrimeNumber/SerchPrimeNumber.java
@@ -16,36 +16,52 @@ public class SerchPrimeNumber {
         int answer = 0;
         set = new HashSet<>();
         
+        
         for(int i=0;i<numbers.length();i++) {
-            ArrayList<Integer> check = new ArrayList<>();
-            check.add(i);
+            boolean[] check = new boolean[numbers.length()];
+            check[i] = true;
             dfs(numbers,Integer.toString(numbers.charAt(i) - '0'),check);    
         }
         
-        
-        
+        answer = set.size();
+        System.out.println(answer);
         return answer;
     }
 	
-	 public static void dfs(String numbers,String position,ArrayList<Integer> check) {
-		 	while(true) {
-		 		if(position.charAt(0) == '0') {
-		 			position.replaceFirst("", "");
-		 		} else {
-		 			break;
-		 		}
-		 	}
+	 public static void dfs(String numbers,String position,boolean[] check) {
 		 	
-	        set.add(Integer.parseInt(position));
-	        
-	        for(int i=0;i<numbers.length();i++) {
-	            if(!check.contains(i)) {
-	                position += numbers.charAt(i) - '0';
-	                check.add(i);
-	                dfs(numbers,position,check);
-	            }
-	        }
-	    }
+	 	if(position.charAt(0) == '0') return;
+	 	
+	 	if(primeCheck((Integer.parseInt(position))) == false) set.add(Integer.parseInt(position));
+	 	
+	 	if(position.length() == numbers.length()) return;
+        
+        for(int i=0;i<numbers.length();i++) {
+            if(check[i] == false) {
+                position += numbers.charAt(i) - '0';
+                check[i] = true;;
+                dfs(numbers,position,check);
+                check[i] = false;
+                position = position.substring(0,position.length()-1);
+                
+            }
+        }
+	 }
+	 
+	 public static boolean primeCheck(int num) {
+		 boolean[] primecheck = new boolean[num+1];
+		 
+		 primecheck[0] = true;
+		 primecheck[1] = true;
+		 
+		 for(int i=2;i<=num/2+1;i++) {
+			 for(int ii=i*2;ii<=num;ii+=i) {
+				 primecheck[ii] = true;
+			 }
+		 }
+		 
+		 return primecheck[num];
+	 }
 	
 
 }
