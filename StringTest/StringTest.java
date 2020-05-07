d7d6b9e0e87118240408290592dd91c2be16bd13
yunut <somewheres@nate.com>
2020-05-07 20:58:14 +0900

    update

---
+++ StringTest/StringTest.java
@@ -0,0 +1,21 @@
+package StringTest;
+
+import java.util.Scanner;
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.nextLine());
+		String st = sc.nextLine();
+		
+		solution(n,st);
+	}
+	
+	public static void solution(int n,String st) {
+		String[] tmp = st.split(" ");
+		
+	}
+
+}
